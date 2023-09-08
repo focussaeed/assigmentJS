c3f4646ca6bb43180ab6992629d0e38346e0eede
focussaeed <138658687+focussaeed@users.noreply.github.com>
2023-09-08 11:16:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,155 @@
+// Chapter No.1;
+
+// Q.No 1;
+
+alert (" Chapter No.1");
+alert ("First Name: Saeed Ahmed");
+alert ("Last Name: Siddiqui");
+alert ("Email: focussaeed@gmail.com");
+alert ("Phone No : 0313-0245701");
+alert ("Password: xxxxxxxxx");
+
+// Q.No 2;
+
+
+alert ("You're Learning JavaScript!")
+
+// Q.No 3;
+
+
+alert ("This is first assignment of learning JavaScript!")
+
+// Chapter No.2;
+
+alert (" Chapter No.2");
+
+// Q.No 1;
+
+alert ("var orderQty");
+
+// Q.No 2;
+
+var myName;
+myName="Saeed Ahmed Siddiqui";
+
+// Q.No 3;
+
+var teamName="Pakistan";
+alert ("Team Name : "+teamName);
+
+// Q.No 4;
+
+var bestMan="Chrlie"
+var bestLeader="Quaid-e-Azam"
+
+// Chapter No.3;
+
+alert (" Chapter No.3");
+
+// Q.No 1;
+
+var caseQty;
+console.log(caseQty);
+
+// Q.No 2;
+
+var caseQty=144;
+console.log(caseQty);
+
+alert(caseQty);
+
+// Q.No 3;
+
+var num="9";
+console.log(num);
+var num2=num-5;
+console.log(num2);
+
+alert(num2);
+
+// Q.No 4;
+
+var abc;
+var abc=10+15;
+alert(abc);
+
+// Q.No 5;
+
+var merchTotal=100;
+var shippingCharge=10;
+var orderTotal=merchTotal+shippingCharge;
+alert("Order Total is "+orderTotal);
+
+// Q.No 6;
+
+var num3=10;
+alert(num3);
+var num3=num3+10;
+alert(num3);
+
+// Chapter No.4;
+alert (" Chapter No.4");
+// Q.No 1;
+
+var productCost=3.45;
+alert("productCode");
+
+// Q.No 2;
+
+var nameOfBand;
+alert("nameOfBand");
+
+// Q.No 3;
+
+var qtyOrdered=10;
+
+// Q.No 4;
+
+var firstName="Saeed Ahmed";
+var lastName="Siddiqui";
+alert(firstName+" "+lastName);
+
+// Q.No 5;
+document.write("Legal variabels<br><br>"+ "var myNam; <br>var firstname; <br>var Lastname18; <br>var Eng_marks; <br>var $agelimit;");
+
+document.write("<br>Illegal variabels<br><br>" + "var 123myName; <br>var @firstname; <br>var Last name; <br>var 1234; <br>var alert;");
+
+// Chapter No.5;
+document.write("<br> Chapter No.5");
+
+// Q.No 1;
+document.write("<br> % is used to give reminder  ");
+
+// Q.No 2;
+
+var num=20%6
+document.write("<br> Value of num is  "+num);
+
+// Q.No 3;
+
+var largeNum=1000;
+var result=largeNum*2000;
+document.write("<br> Resut of large number after multiply by 2000    "+result);
+
+// Q.No 4;
+var xy=20;
+var ab=10;
+var x=xy-ab;
+
+alert(x);
+
+// Q.No 5;
+
+var x=20;
+var y=6;
+var r=x%y;
+alert(r);
+
+
+// Q.No 6;
+
+var x=20;
+var y=6;
+var r=x*y;
+alert(r);
+
